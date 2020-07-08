5bcfa7abe6063d546bb8746a675d4b0cacfdef61
Everton Correa de Borba <everton.borba@gmail.com>
2020-07-07 22:24:39 -0300

    IGTI - Desafio do Módulo 4

---
+++ grades-api/models/grades.js
@@ -0,0 +1,28 @@
+export default (mongoose) => {
+  const schema = mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    subject: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: Number,
+      required: true,
+    },
+    lastModified: {
+      type: String,
+      required: true,
+    },
+  });
+
+  const Grades = mongoose.model('grades', schema, 'grades');
+
+  return Grades;
+};
